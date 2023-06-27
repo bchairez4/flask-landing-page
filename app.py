187ba910d556e1002358481292b12577424b322b
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-06-26 20:09:30 -0700

    updated app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 def main():
-    pass
+    app.run()
 
 if __name__ == '__main__':
     main()
