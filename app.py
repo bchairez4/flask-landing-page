1cb02c3d9e38ea83308dcb20c90707d936cc36b8
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-06-28 21:49:53 -0700

    updated app.py

--- app.py
+++ app.py
@@ -2,9 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+navbarList = ['Home', 'Adopt', 'About']
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', navbarList=navbarList)
+
+@app.route('/adopt')
+def adopt():
+    adoptionList = [{'img_src': 'link', 'animal': 'dog', 'breed': 'doggy', 'name': 'name', 'age': 0},
+                    {'img_src': 'link', 'animal': 'dog', 'breed': 'doggy', 'name': 'name', 'age': 0},
+                    {'img_src': 'link', 'animal': 'dog', 'breed': 'doggy', 'name': 'name', 'age': 0},
+                    {'img_src': 'link', 'animal': 'dog', 'breed': 'doggy', 'name': 'name', 'age': 0},
+                    {'img_src': 'link', 'animal': 'dog', 'breed': 'doggy', 'name': 'name', 'age': 0}]
+    return render_template('adopt.html', navbarList=navbarList, adoptionList=adoptionList)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', navbarList=navbarList)
 
 def main():
     app.run()
