72905a554da4e2f82c306aed8910bd9fc989d18a
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-07-01 14:03:34 -0700

    updated app.py

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ def index():
 
 @app.route('/adopt')
 def adopt():
-    adoptionList = [{'img_src': 'link', 'animal': 'dog', 'breed': 'doggy', 'name': 'name', 'age': 0},
-                    {'img_src': 'link', 'animal': 'dog', 'breed': 'doggy', 'name': 'name', 'age': 0},
-                    {'img_src': 'link', 'animal': 'dog', 'breed': 'doggy', 'name': 'name', 'age': 0},
-                    {'img_src': 'link', 'animal': 'dog', 'breed': 'doggy', 'name': 'name', 'age': 0},
-                    {'img_src': 'link', 'animal': 'dog', 'breed': 'doggy', 'name': 'name', 'age': 0}]
+    adoptionList = [{'img_src': 'static/images/adopt-animal-1.jpg', 'animal': 'dog', 'breed': 'doggy', 'name': 'Fluffy', 'age': 1},
+                    {'img_src': 'static/images/adopt-animal-2.jpg', 'animal': 'dog', 'breed': 'doggy', 'name': 'Softy', 'age': 2},
+                    {'img_src': 'static/images/adopt-animal-3.jpg', 'animal': 'dog', 'breed': 'doggy', 'name': 'Bruce', 'age': 4},
+                    {'img_src': 'static/images/adopt-cat-1.jpg', 'animal': 'cat', 'breed': 'kitty', 'name': 'Nightfall', 'age': 3},
+                    {'img_src': 'static/images/adopt-cat-2.jpg', 'animal': 'cat', 'breed': 'kitty', 'name': 'Bucky', 'age': 5},
+                    {'img_src': 'static/images/adopt-cat-3.jpg', 'animal': 'cat', 'breed': 'kitty', 'name': 'Mao-San', 'age': 3}]
     return render_template('adopt.html', navbarList=navbarList, adoptionList=adoptionList)
 
 @app.route('/about')
