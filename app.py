5ea4e279b88b5be6ac66e936245d15a0de9e7ae8
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-07-03 14:44:14 -0700

    removed navbarList as a dependency
    Moving navbar declaration to layout.html

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-navbarList = ['Home', 'Adopt', 'About']
-
 @app.route('/')
 def index():
-    return render_template('index.html', navbarList=navbarList)
+    return render_template('index.html')
 
 @app.route('/adopt')
 def adopt():
@@ -19,14 +17,14 @@ def adopt():
                     {'img_src': 'static/images/adopt-animal-4.jpg', 'animal': 'cat', 'breed': 'kitty', 'name': 'Nightfall', 'age': 3},
                     {'img_src': 'static/images/adopt-animal-5.jpg', 'animal': 'cat', 'breed': 'kitty', 'name': 'Bucky', 'age': 5},
                     {'img_src': 'static/images/adopt-animal-6.jpg', 'animal': 'cat', 'breed': 'kitty', 'name': 'Mao-San', 'age': 3}]
-    return render_template('adopt.html', navbarList=navbarList, adoptionList=adoptionList)
+    return render_template('adopt.html', adoptionList=adoptionList)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', navbarList=navbarList)
+    return render_template('about.html')
 
 def main():
-    app.run()
+    app.run(debug="True")
 
 if __name__ == '__main__':
     main()
